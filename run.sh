ada398c0f530dbf3c84f4eae39eff3bc59dada15
sudogreg <sudogreg@gmail.com>
2017-11-12 14:51:48 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 # bug fix for case sensitive filesystems
 # without this myhs.homeseer.com wont load icons
 ln -s /HomeSeer/html/images/homeseer /HomeSeer/html/images/HomeSeer
+cp /HomeSeer/Data/PLUGINS/HSPI_*.* /HomeSeer
 
 mono /HomeSeer/HSConsole.exe --log
