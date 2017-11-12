2bda6748a30cebe68459e33955ae40154300baf9
sudogreg <sudogreg@gmail.com>
2017-11-12 16:58:22 -0600

    Update run.sh
    create override to move data to root /HomeSeer dir (updater_override.txt) and zips to move data to Updates3/Zips (zipped plugin)

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # bug fix for case sensitive filesystems
 # without this myhs.homeseer.com wont load icons
 ln -s /HomeSeer/html/images/homeseer /HomeSeer/html/images/HomeSeer
-cp -n /HomeSeer/Data/PLUGINS/HSPI_*.exe /HomeSeer
-sleep 3
-
+mv /HomeSeer/Data/PLUGINS/override/*.* /HomeSeer
+mv /HomeSeer/Data/PLUGINS/zips/*.* /HomeSeer/Updates3/Zips
+sleep 2
 mono /HomeSeer/HSConsole.exe --log
